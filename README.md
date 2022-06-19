ebc25a3922f77d052df9b3a9ddaf696ce2f0cce0
cycycy_kwong <kwongcheukyin@gmail.com>
2022-06-19 23:46:38 +0800

    update Dockerfile and readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# search-dishes-graphql-federation
+# Simple Dish Searching application - Kotlin DGS Framwork Federation example
+
+This repository builds a simple dish searching application with the DGS Framework.
+
+It demonstrates the basic features of GraphQL and makes used of a federation gateway.
+
+## To get started
+
+The example requires Java 17. Build the docker images of restaurant, dish and federation gateway services in an IDE. Then run the following command from the root folder:
+
+    docker-compose up -d
+
+Interact with the application on http://localhost:9000
--- restaurant-be/src/main/kotlin/com/cycycykwong/restaurant/datafetcher/RestaurantDataFetcher.kt
+++ restaurant-be/src/main/kotlin/com/cycycykwong/restaurant/datafetcher/RestaurantDataFetcher.kt
@@ -3,13 +3,27 @@ package com.cycycykwong.restaurant.datafetcher
 import com.cycycykwong.generated.types.Restaurant
 import com.cycycykwong.restaurant.service.RestaurantService
 import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsDataFetchingEnvironment
+import com.netflix.graphql.dgs.DgsEntityFetcher
 import com.netflix.graphql.dgs.DgsQuery
+import org.dataloader.DataLoader
 import org.springframework.beans.factory.annotation.Autowired
+import java.util.*
+import java.util.concurrent.CompletableFuture
 
 @DgsComponent
 class RestaurantDataFetcher(@Autowired val restaurantService: RestaurantService) {
     @DgsQuery
     fun restaurants(): List<Restaurant> {
         return restaurantService.restaurants()
+    }
+
+    @DgsEntityFetcher(name = "Restaurant")
+    fun restaurant(
+        values: Map<String, Objects>,
+        dfe: DgsDataFetchingEnvironment
+    ): CompletableFuture<Restaurant> {
+        val dataLoader: DataLoader<String, Restaurant> = dfe.getDataLoader("restaurant")
+        return dataLoader.load(values["id"].toString())
     }
 }
